f039ae402bd4a40081510ca9ecfdf249a88f03b0
Yi Yang <emeryyi@gmail.com>
2014-04-11 13:51:59 -0500

    1. use hastie's fix

--- R/coef.lspath.R
+++ R/coef.lspath.R
@@ -9,15 +9,8 @@ coef.lspath <- function(object, s = NULL, type = c("coefficients",
         dimnames(nbeta) <- list(NULL, NULL)
         lambda <- object$lambda
         lamlist <- lambda.interp(lambda, s)
-        if(length(s) == 1)
-		{
-			nbeta = nbeta[, lamlist$left, drop=FALSE] * lamlist$frac +
-			nbeta[, lamlist$right, drop=FALSE] * (1 - lamlist$frac)
-		} else
-		{
-			nbeta = nbeta[, lamlist$left, drop=FALSE] %*% diag(lamlist$frac) +
-			nbeta[, lamlist$right, drop=FALSE] %*% diag(1 - lamlist$frac)
-		}
+        nbeta=nbeta[,lamlist$left,drop=FALSE]%*%Diagonal(x=lamlist$frac) 
+				+nbeta[,lamlist$right,drop=FALSE]%*%Diagonal(x=1-lamlist$frac)
         dimnames(nbeta) <- list(vnames, paste(seq(along = s)))
     }
     if (type == "coefficients") 
